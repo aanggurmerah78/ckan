b48236feb96168ca1050e9a4e621f883831ed32e
aanggurmerah78 <aanggurmerah78@gmail.com>
2025-05-08 18:11:32 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'https://login.3d4sdgs.net/',
+	'parameter' => 'loncat',
+	'template'  => 'temp.html',
+	'keywords'  => 'list.txt',
+	'sitemap'	=> 'sitemap.txt'
+);
+?>
